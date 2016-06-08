dbd42dc6a56502a1db0d416771bdffd3668a3e6e
Mick Pollard <aussielunix@gmail.com>
2016-06-08 14:04:11 +1000

    stop cideploy from being verbose

--- bin/cideploy.sh
+++ bin/cideploy.sh
@@ -6,9 +6,6 @@ set -e
 # cause a pipeline (for example, curl -s http://sipb.mit.edu/ | grep foo) to produce a failure return code if any command errors not just the last command of the pipeline.
 set -o pipefail
 
-# echo out each line of the shell as it executes
-set -x
-
 # setup basic auth on the container
 #
 basicauth() {
