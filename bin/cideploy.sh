c9a6b296f9fe34b7eb771c14240f0350c3438011
Mick Pollard <aussielunix@gmail.com>
2016-06-08 12:52:08 +1000

    migrate deploy commands from circle.yml to bin/cideploy.sh

---
+++ bin/cideploy.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Exit immediately if there is an error
+set -e
+
+# cause a pipeline (for example, curl -s http://sipb.mit.edu/ | grep foo) to produce a failure return code if any command errors not just the last command of the pipeline.
+set -o pipefail
+
+# echo out each line of the shell as it executes
+set -x
+
+# setup basic auth on the container
+#
+basicauth() {
+  if [[ -n ${CF_BASIC_AUTH_PASSWORD+x} ]]
+  then
+    htpasswd -cb site/Staticfile.auth $CF_BASIC_AUTH_USERNAME $CF_BASIC_AUTH_PASSWORD
+  else
+    echo "Not setting a password."
+  fi
+}
+
+# main script function
+#
+main() {
+  readonly GITBRANCH="${CIRCLE_BRANCH}"
+
+  case "${GITBRANCH}" in
+    master)
+      appname="cgadocs"
+      ;;
+    *)
+      appname="cgadocs-${GITBRANCH}"
+      ;;
+  esac
+
+  basicauth
+  cf api $CF_API
+  cf auth $CF_USER $CF_PASSWORD
+  cf target -o $CF_ORG
+  cf target -s $CF_SPACE
+  cf push $appname
+}
+
+main $@
