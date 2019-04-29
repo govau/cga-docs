8119ca521228b99798e17498a492cbe8d175b3f8
Brendan Gordon <brendan.gordon@digital.gov.au>
2019-04-29 12:37:04 +1000

    Add needed dirs for hugo

---
+++ site/themes/designsystem.gov.au/README.md
@@ -0,0 +1,19 @@
+
+This theme could be moved to its own repo and used as a git submodule
+if it becomes useful to other projects.
+
+## designsystem theme cheatsheet
+
+```
+cd src
+
+# Install all dependencies
+npm install
+
+# Add new module
+npm install @gov.au/foo
+
+# Update a module
+npm update @gov.au/foo
+```
+
---
+++ site/themes/designsystem.gov.au/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Exit immediately if there is an error
+set -e
+
+# cause a pipeline (for example, curl -s http://sipb.mit.edu/ | grep foo) to produce a failure return code if any command errors not just the last command of the pipeline.
+set -o pipefail
+
+# echo out each line of the shell as it executes
+set -x
+
+# cd to the src dir
+cd "$( dirname "${BASH_SOURCE[0]}" )/src"
+
+# install the packages
+npm install
+
+# build the theme
+npm run-script build
